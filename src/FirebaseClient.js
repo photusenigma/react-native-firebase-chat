aa852a52bbcf958f5a27fb921830c8a8c57c869e
photusenigma <dcw@Davids-MacBook-Pro.local>
2017-02-10 15:09:49 -0500

    update to use personal firebase project and static apiKey.

--- src/FirebaseClient.js
+++ src/FirebaseClient.js
@@ -1,12 +1,13 @@
 const firebase = require('firebase')
-import { API_KEY } from 'react-native-dotenv'
+    // import { API_KEY } from 'react-native-dotenv'
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: "dailydrip-firebase-chat.firebaseapp.com",
-  databaseURL: "https://dailydrip-firebase-chat.firebaseio.com",
-  storageBucket: "dailydrip-firebase-chat.appspot.com"
+    apiKey: "AIzaSyCdqvrAF9Mztpfy6rCxAJX3AAxG4oLoSV8",
+    authDomain: "simplechat-9e92b.firebaseapp.com",
+    databaseURL: "https://simplechat-9e92b.firebaseio.com",
+    storageBucket: "simplechat-9e92b.appspot.com",
+    messagingSenderId: "134578640171"
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
